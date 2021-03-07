b9ebf4d3eb8f0014b739cc547b28571a60cfa6ef
Artem <artem.hamuha@gmail.com>
2021-03-06 22:51:15 -0800

    all my previous work

---
+++ src/artem.py
@@ -0,0 +1,20 @@
+#Initalize another variable with a user-specified value.
+
+user=input('I am python.What is your name? :')
+
+#Out put a string and a variable value.
+
+print('Welcome',user)
+
+a=18
+
+b=12
+
+print('Assing Values:\t\t','\tb=',b)
+
+print('Add and Assing:\t\t','a=',a,(8+4)
+
+
+
+
+
---
+++ src/artem_scope.py
@@ -0,0 +1,13 @@
+A= 10
+print ('A=',A)
+B=20
+print ('B=',B)
+
+def my_vars() :
+	A = 40
+	B=40
+	print('Local Variables:' , A,B)
+my_vars()
+
+print('Global B:' ,A, B)
+																																																																																																																					
