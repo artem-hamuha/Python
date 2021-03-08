c047b964db36aeedc4ad9bcc289428c64ce07514
Artem <artem.hamuha@gmail.com>
2021-03-07 21:22:55 -0800

    Just a random idea, basically a information gatherer func.

--- src/random.py
+++ src/random.py
@@ -1,1 +1,9 @@
-print("yes")
+def welcome():
+    print("Welcome to TWC\nThe Welcome Center")
+
+    name = input("We will start with your name - ")
+    full_name = input("Now your full name " + name +" -")
+    age = input("And how old are you? - ")
+    birth = input("Ok, and your birth date - ")
+
+    print("Well, thank you so much " + name + " your information is safe with us.")
