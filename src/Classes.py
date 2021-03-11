b802bcfdcdeceaa7e7ddec1ba2fb65e4cc21d740
Artem <artem.hamuha@gmail.com>
2021-03-10 21:38:54 -0800

    Trying classes

---
+++ src/Classes.py
@@ -0,0 +1,18 @@
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return (self.width + self.height) * 2
+
+    def __str__(self):
+        return "Rectangle: width {}, height {} ".format(self.width, self.height)
+
+    def __eq__(self, other):
+        return self.width == other.width and self.height == other.height
+
+
