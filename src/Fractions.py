c9f243a3183161f1ff3709a3cb6d1bad6ae666e4
Artem <artem.hamuha@gmail.com>
2021-03-19 18:55:14 -0700

    Looking at random and fraction package

---
+++ src/Fractions.py
@@ -0,0 +1,25 @@
+import fractions
+
+# As float
+print(fractions.Fraction(1.1))
+
+# As string
+print(fractions.Fraction('1.1'))
+
+import random
+
+print(random.randrange(10, 20))
+
+x = ['a', 'b', 'c', 'd', 'e']
+
+# Get random choice
+print(random.choice(x))
+
+# Shuffle x
+random.shuffle(x)
+
+# Print the shuffled x
+print(x)
+
+# Print random element
+print(random.random())
