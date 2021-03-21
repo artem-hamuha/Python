62ff61d0735d3810abdceaf08a1c216cd2ebbf8c
Artem <artem.hamuha@gmail.com>
2021-03-20 21:23:32 -0700

    Some integer operations

---
+++ src/Num_Operations.py
@@ -0,0 +1,15 @@
+num = 3.5
+num2 = 3.4
+
+print(round(num), ",", round(num2))  # this works well
+
+num = 3.5678153
+num2 = 3.4357689
+
+print(round(num, 2), ",", round(num2, 2))  # you can also use round() to round from any decimal place
+
+print(abs(3))  # abs stands for absolute value
+print(abs(-5))  # it always returns a positive
+
+print(pow(3, 2))  # this says three to the power of two
+# pow means power as if raise to the power of
