c644d2fc304a0497285b086209560ca33e0e70fc
Artem <artem.hamuha@gmail.com>
2021-03-27 10:30:35 -0700

    Added to more functions to yesterdays commit

--- src/Numbers.py
+++ src/Numbers.py
@@ -24,3 +24,30 @@ def from_base10(n, b):
 print(from_base10(10, 2))
 
 print(from_base10(255, 16))
+
+
+def encode(digits, digit_map):
+    if max(digits) >= len(digit_map):
+        raise ValueError("digit_map is not long enough to encode the digits")
+    return ''.join([digit_map[d] for d in digits])
+
+
+print(encode([15, 15], '0123456789ABCDEF'))
+
+
+def rebase_from10(number, base):
+    digit_map = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    if base < 2 or base > 36:
+        raise  ValueError('Invalid base: must be in range of 2 - 36')
+    sign = -1 if number < 0 else 1
+    number *= sign
+    digits = from_base10(number, base)
+    encoding = encode(digits, digit_map)
+    if sign == -1:
+        encoding = '-' + encoding
+    return encoding
+
+
+e = rebase_from10(10, 2)  # should get 1010(binary)
+print(e)
+print(int(e, base=2))  # should get 10
