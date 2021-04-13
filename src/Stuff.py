17bea37e74ffa8b93cd1dd2d2e4fc244e1a5697e
Artem <artem.hamuha@gmail.com>
2021-04-12 19:16:17 -0700

    Doing a for loop with break

--- src/Stuff.py
+++ src/Stuff.py
@@ -7,3 +7,13 @@ minutes = time // 60
 time %= 60
 seconds = time
 print("d:h:m:s-> %d:%d:%d:%d" % (day, hour, minutes, seconds))
+
+numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9) # Declaring the tuple
+num_sum = 0
+count = 0
+for x in numbers:
+    num_sum = num_sum + x
+    count = count + 1
+    if count == 5:
+        break
+print("Sum of first ",count,"integers is: ", num_sum)
