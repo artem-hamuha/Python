ce5790a8a610d52801ea44bbd893ebd188381522
Artem <artem.hamuha@gmail.com>
2021-04-28 12:13:51 -0700

    truncation

---
+++ src/Trunk.py
@@ -0,0 +1,15 @@
+from math import trunc
+from math import floor
+from math import ceil
+
+print(int(10.6), int(10.9), int(10.3))
+
+print(floor(10.6), floor(10.9), floor(10.3))
+
+print(trunc(10.6), trunc(10.9), trunc(10.3))
+
+print(trunc(-10.6), trunc(-10.9), trunc(-10.3))
+
+print(floor(-10.6), floor(-10.9), floor(-10.3))
+
+print(ceil(-10.6), ceil(-10.9), ceil(-10.3))
