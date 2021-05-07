4b471e520e3eed93b9aa8de707403f9a624c2eb2
Artem <artem.hamuha@gmail.com>
2021-05-06 20:07:15 -0700

    practice

---
+++ src/Decimals2.py
@@ -0,0 +1,6 @@
+def func(num1, num2, num3, num4, num5, name):
+    num = num1 + num2 + num3 + num4 + num5
+    return "Name is - {}, sum of numbers is - {}".format(name, num)
+
+
+print(func(1, 4, 8, 8, 3, "Dude"))
